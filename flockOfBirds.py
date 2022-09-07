8e059da56e6f0bdd8997a970af138dfc1f895d80
safimuhammad <safiwahid38@gmail.com>
2022-09-08 03:30:58 +0500

    description

--- flockOfBirds.py
+++ flockOfBirds.py
@@ -1,2 +1,3 @@
 # Initializing the project
 # Main contributors of this repository are safimuhammad and khizarsiddiqui
+# flock of birds simulation will be carried out by using the boids simulation model.
