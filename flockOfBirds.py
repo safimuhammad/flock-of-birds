1ac35d790738bf532f05e831ef41edfc8f515b4d
safimuhammad <safiwahid38@gmail.com>
2022-09-09 03:22:18 +0500

    description added

--- flockOfBirds.py
+++ flockOfBirds.py
@@ -1,3 +1,4 @@
 # Initializing the project
 # Main contributors of this repository are safimuhammad and khizarsiddiqui
 # flock of birds simulation will be carried out by using the boids simulation model.
+# The boids simulation of motion consists of four rules
