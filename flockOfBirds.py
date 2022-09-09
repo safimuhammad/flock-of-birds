2ff2a5bb1dcf7085faefefe060aeff5f5c94d83f
safimuhammad <safiwahid38@gmail.com>
2022-09-10 01:48:34 +0500

    adding the initial postion constraints

--- flockOfBirds.py
+++ flockOfBirds.py
@@ -1,4 +1,12 @@
-# Initializing the project
-# Main contributors of this repository are safimuhammad and khizarsiddiqui
-# flock of birds simulation will be carried out by using the boids simulation model.
-# The boids simulation of motion consists of four rules
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+width, height  = 640 , 480
+startingPoint= 2.0
+N = 20
+pos = [width/startingPoint,height/startingPoint] + 10*np.random.rand(2*N).reshape(N,2)
+angles = 2*math.pi*np.random.rand(N)
+vel = np.array(list(zip(np.sin(angles), np.cos(angles))))
+plt.imshow(pos, interpolation='nearest')
+plt.show()
