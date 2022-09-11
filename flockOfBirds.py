452b404460ed9aa1de09df7102b6bc12afb0589d
safimuhammad <safiwahid38@gmail.com>
2022-09-12 01:31:09 +0500

    testing boundries

--- flockOfBirds.py
+++ flockOfBirds.py
@@ -2,12 +2,31 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-width, height  = 640 , 480
-startingPoint= 2.0
-N = 20
-pos = [width/startingPoint,height/startingPoint] + 10*np.random.rand(2*N).reshape(N,2)
-angles = 2*math.pi*np.random.rand(N)
-vel = np.array(list(zip(np.sin(angles), np.cos(angles))))
-plt.imshow(pos, interpolation='nearest')
-plt.show()
-#drawing the boids now 
+
+def boidPosition():
+    width, height  = 640 , 480
+    startingPoint= 2.0
+    N = 20
+    pos = [width/startingPoint,height/startingPoint] + 10*np.random.rand(2*N).reshape(N,2)
+    angles = 2*math.pi*np.random.rand(N)
+    vel = np.array(list(zip(np.sin(angles), np.cos(angles))))
+
+
+def applyBoundries(pos):
+    testVal = []
+    deltaR = 2.0
+    
+
+    for coords in pos:
+        if coords[0]> width + deltaR:
+            coords[0] = -deltaR
+        if coords[0] < - deltaR:
+            coords[0] = width + deltaR
+        if coords[1] > height + deltaR:
+            coords[1] = - deltaR
+        if coords[1]  < - deltaR:
+            coords[1] = height + deltaR
+        
+
+
+
